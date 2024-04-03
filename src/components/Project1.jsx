e3085696c937852e23a33b95d786b4bcab1e91b4
James Green <jmgreen1202@gmail.com>
2024-04-03 16:28:57 -0700

    commit

--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -1,8 +1,7 @@
 import styles from '../styles/Project1.module.css';
 
 function Project1() {
-  const project1ColabLink = "https://JamesGreen1202.github.io/portfolio/YourNotebook1.html";
-
+  const project1ColabLink = "https://JamesGreen1210.github.io/portfolio/Welcome_To_Colaboratory.html";
   return (
     <div className={styles.project1}>
       <h1>Quantum Machine Learning Project</h1>
