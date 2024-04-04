c6201ea3ddb69e55aa1b65cc09867086f243e730
James Green <jmgreen1202@gmail.com>
2024-04-04 12:13:11 -0700

    pushing before work

--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -1,17 +1,22 @@
+import React, { useEffect } from 'react';
 import styles from '../styles/Project1.module.css';
 
 function Project1() {
   const project1ColabLink = "https://JamesGreen1210.github.io/portfolio/Welcome_To_Colaboratory.html";
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className={styles.project1}>
-      <h1>Quantum Machine Learning Project</h1>
-      {/* ... rest of your content */}
-      <p>What issues have I ran into?</p>
-      {/* ... rest of your content */}
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
+      <div className={styles.header}>Quantum Hybrid Project</div>
+      <div className={styles.buttonContainer}>
+        <a href={project1ColabLink} target="_blank" rel="noopener noreferrer" className={styles.button}>Open in Colab</a>
+      </div>
+      <div className={styles.iframeContainer}>
         <iframe 
           src={project1ColabLink} 
-          style={{ width: "80%", height: "2000px", border: "2px solid black" }} 
           frameborder="0" 
           allowfullscreen 
           title="Google Colab Notebook"
